a3a1e208ce98ee2012879908dee0b642f5678a98
kirillanpilov <anpilov.kirill2002@gmail.com>
2023-05-03 21:51:37 +0300

    JavaDoc for MvcConfig

--- src/main/java/com/anpilov/onlinebank/config/MvcConfig.java
+++ src/main/java/com/anpilov/onlinebank/config/MvcConfig.java
@@ -4,6 +4,21 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+
+
+/**
+
+ This class represents the configuration for Spring MVC, which is used to
+ configure the web application with Spring Framework. It implements the
+ WebMvcConfigurer interface to provide custom configuration options.
+ The class adds view controllers to the registry, which allow the mapping of
+ URLs to views without the need for a controller.
+ @author Anpilov Kirill
+ @version 1.0
+ @since
+ */
+
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
